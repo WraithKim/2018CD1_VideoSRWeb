dcce5e0f4d9f8464147219bacf749413d09df354
Donghwan Kim <wraithtorch@gmail.com>
2018-10-06 17:01:42 +0000

    delete_file에 대한 잘못된 url 수정

--- videosr/urls.py
+++ videosr/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('upload/complete/', views.upload_complete, name='upload_complete'),
     path('download/', views.download_test, name='download_test'),
     path('download/<int:pk>/', views.download_file, name='download_file'),
-    path('download/<int:pk>/delete', views.delete_file, name='delete_file'),
+    path('download/<int:pk>/delete/', views.delete_file, name='delete_file'),
 ]
