8bb9ea97b88c7e349c3dc7cf4de80cd92cd7d240
Donghwan Kim <wraithtorch@gmail.com>
2018-10-08 07:21:15 +0000

    SECRET_KEY 분리

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4-redh0opsf798aexb%(a&_zmx!z4!4w31p+m0d$3xm$2fa8uu'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
