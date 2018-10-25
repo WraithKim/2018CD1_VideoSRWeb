d39d3a8a57409dcf7628f98697d1066bc8a4323d
Donghwan Kim <wraithtorch@gmail.com>
2018-10-25 15:27:11 +0000

    create Customer Model when User is created. - add 'create_customer_model()' with post_save signal from User

--- videosr/signals.py
+++ videosr/signals.py
@@ -1,4 +1,6 @@
 from .models import UploadedFile
+from .models import Customer
+from django.contrib.auth.models import User
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 import os, logging
@@ -12,3 +14,10 @@ def delete_uploaded_file(sender, instance, *args, **kwargs):
     path = instance.uploaded_file.path
     if os.path.isfile(path):
         os.remove(path)
+
+@receiver(post_save, sender=User, dispatch_uid='create_user_signal')
+def create_customer_model(sender, instance, created, **kwargs):
+    """create customer model when user is created
+    """
+    if created:
+        Customer.objects.create(user=instance)
