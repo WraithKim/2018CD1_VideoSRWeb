0cff8a42d55d4529b0528357b7c9622c2d83b584
Donghwan Kim <wraithtorch@gmail.com>
2018-11-11 08:23:41 +0000

    sb-admin2라는 템플릿으로 변경, views에서 로그인된 유저의 경우 대쉬보드로 넘겨줌

--- login/views.py
+++ login/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, redirect
 
 def index(request):
     if request.user.is_authenticated:
-        return render(request, 'login/index.html')
+        return redirect('dashboard:index')
     else:
         return render(request, 'login/index.html')
