d41659412bec3c1b608f388386c943e5733319db
Donghwan Kim <wraithtorch@gmail.com>
2018-11-17 16:50:21 +0000

    경로 오류로 인한 다운로드 불가능 버그 해결, 테스트를 위해 SR파일이 아닌 원본 파일을 다운로드 함.  - 다운로드 경로를 줄 때, os.path.split()이 디렉토리 명 뒤에 슬래시를 붙여주지 않아서 발생  - 문자열 합연산이 아닌 os.path.join()을 사용해서 경로를 다시 만드는 걸로 해결함

--- dashboard/views.py
+++ dashboard/views.py
@@ -100,12 +100,14 @@ def upload_complete(request):
 def download_file(request, pk):
     file_to_download = get_object_or_404(UploadedFile, pk=pk, owner=request.user)
     (dirname, basename) = os.path.split(file_to_download.uploaded_file.name)
-    basename = "sr_" + basename
-    if not os.path.exists(settings.MEDIA_ROOT+dirname + basename):
+    # basename = "sr_" + basename #FIXME: test code
+    sr_relative_path = os.path.join(dirname, basename)
+    if not os.path.isfile(settings.MEDIA_ROOT + sr_relative_path):
+        logger.warn("Download file - file not found. pk: {}".format(pk))
         return HttpResponse(status=404)
     response = HttpResponse()
     response['Content-Disposition'] = 'attachment; filename={0}'.format(urllib.parse.quote(file_to_download.uploaded_filename))
-    response['X-Accel-Redirect'] = '/media/{0}'.format(dirname + basename)
+    response['X-Accel-Redirect'] = '/media/{0}'.format(sr_relative_path)
     return response
 
 @login_required
