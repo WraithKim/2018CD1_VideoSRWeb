59d2a09072c1e01a773c7daed0f1b12111917325
Donghwan Kim <wraithtorch@gmail.com>
2018-11-17 03:45:15 +0000

    URL옮기면서 빠진 amount값 추가  - success에 amount 값이 필요한데 제외시켜서 다시 추가함

--- payment/urls.py
+++ payment/urls.py
@@ -5,6 +5,6 @@ app_name = 'payment'
 urlpatterns = [
     path('', views.index, name='index'),
     path('request/<int:amount>/', views.payment_request, name='payment_request'),
-    path('success/',views.payment_success, name='payment_success'),
+    path('success/<int:amount>/',views.payment_success, name='payment_success'),
     path('fail/',views.payment_fail, name='payment_fail'),
 ]
